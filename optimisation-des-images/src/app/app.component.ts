4ac685fd1a814b3a26fff6188a0bdea285dab450
paulcalimache <paul.calimache@thalesaleniaspace.com>
2022-12-19 11:45:10 +0100

    blur effect

--- optimisation-des-images/src/app/app.component.ts
+++ optimisation-des-images/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked{
   title = 'optimisation-des-images';
+
+  private heroImgBefore = '../assets/photo_foret_tiny.webp';
+  private heroImgAfter = '../assets/photo_foret.webp';
+
+  public HeroImg = this.heroImgBefore;
+  public isLoaded = false;
+
+  ngAfterViewChecked() {
+    this.HeroImg = this.heroImgAfter;
+    this.isLoaded = true;
+  }
 }
